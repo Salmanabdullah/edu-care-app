e1903bb528dde70865a7e94f329e044731b96896
Abdullah Al Noman <salmandcc@gmail.com>
2024-05-31 18:33:09 +0200

    modify UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
 import { AuthContextProvider } from "./context/AuthContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>
 );
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -12,77 +12,69 @@ const Signup = () => {
 
   return (
     <>
-      <div className="bg-grey-lighter min-h-screen flex flex-col">
-        <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-            <form className="" onSubmit={handleSubmit}>
-              <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-              {/* <input
-              type="text"
-              className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="fullname"
-              placeholder="Full Name"
-            /> */}
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-100">
+            Sign Up
+          </h2>
+        </div>
 
-              <input
-                type="text"
-                className="block border border-grey-light w-full p-3 rounded mb-4 text-white"
-                name="email"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label className="block text-2xl leading-6 text-gray-100">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-              <input
-                type="password"
-                className="block border border-grey-light w-full p-3 rounded mb-4 text-white"
-                name="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-              {/* <input
-              type="password"
-              className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="confirm_password"
-              placeholder="Confirm Password"
-            /> */}
+            <div>
+              <div className="flex items-center justify-between">
+                <label className="block text-xl leading-6 text-gray-100">
+                  Password
+                </label>
+                
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
+            <div>
               <button
                 type="submit"
-                className="w-full text-center py-3 rounded bg-green-400 text-black hover:bg-green-dark focus:outline-none my-1"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Create Account
+                Sign Up
               </button>
-            </form>
-            <div className="text-center text-sm text-grey-dark mt-4">
-              By signing up, you agree to the
-              <a
-                className="no-underline border-b border-grey-dark text-grey-dark"
-                href="#"
-              >
-                Terms of Service
-              </a>{" "}
-              and
-              <a
-                className="no-underline border-b border-grey-dark text-grey-dark"
-                href="#"
-              >
-                Privacy Policy
-              </a>
             </div>
-          </div>
+          </form>
 
-          <div className="text-grey-dark mt-6">
-            Already have an account?
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?
             <a
-              className="no-underline border-b border-blue text-blue"
-              href="../login/"
-            >
-              Log in
-            </a>
-            .
-          </div>
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            ></a>
+          </p>
         </div>
       </div>
     </>
