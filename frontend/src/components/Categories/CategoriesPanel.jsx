9ab6d76316c0b0b5d10b6006947e2a826a5f55b9
Abdullah Al Noman <salmandcc@gmail.com>
2024-06-04 21:04:26 +0200

    map alignments

--- frontend/src/components/Categories/CategoriesPanel.jsx
+++ frontend/src/components/Categories/CategoriesPanel.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 
 const CategoriesPanel = () => {
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    //await login(req,res);
+    
+    
+    console.log(e.target.innerText);
+  };
   return (
     <div className="">
       <ul className="flex space-x-10 justify-center text-xl p-4 f">
         <li>
-          <button>School</button>
+          <button onClick={handleClick}>School</button>
         </li>
         <li>
-          <button>Kindergarten</button>
+          <button onClick={handleClick}>Kindergarten </button>
         </li>
         <li>
-          <button>Child</button>
+          <button onClick={handleClick}>Child</button>
         </li>
         <li>
-          <button>Teenager</button>
+          <button onClick={handleClick}>Teenager</button>
         </li>
       </ul>
     </div>
